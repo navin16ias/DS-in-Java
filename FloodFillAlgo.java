868eaea2a8e0c1e76d40ce1dcc7284103e96009f
navin16ias <navin16ias@gmail.com>
2020-02-07 09:29:39 +0530

    Add files via upload

---
+++ FloodFillAlgo.java
@@ -0,0 +1,53 @@
+package com.test.tdd;
+
+public class FloodFillAlgo {
+
+	static int M = 8;
+	static int N = 8;
+
+	public static void main(String[] args) {
+		int screen[][] = { { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1, 0, 0 }, { 1, 0, 0, 1, 1, 0, 1, 1 },
+				{ 1, 2, 2, 2, 2, 0, 1, 0 }, { 1, 1, 1, 2, 2, 0, 1, 0 }, { 1, 1, 1, 2, 2, 2, 2, 0 },
+				{ 1, 1, 1, 1, 1, 2, 1, 1 }, { 1, 1, 1, 1, 1, 2, 2, 1 }, };
+		int x = 4, y = 4, newC = 3;
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++)
+				System.out.print(screen[i][j] + " ");
+			System.out.println();
+		}
+		
+		floodFill(screen, x, y, newC);
+
+		System.out.println("Updated screen after call to floodFill: ");
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++)
+				System.out.print(screen[i][j] + " ");
+			System.out.println();
+		}
+	}
+
+	public static void floodFill(int[][] screen, int x, int y, int newC) {
+		int prevC = screen[x][y];
+		floodFill(screen, x, y, prevC, newC);
+
+	}
+
+	public static void floodFill(int[][] screen, int x, int y, int prevC, int newC) {
+		if (x < 0 || x >= M || y < 0 || y >= N) {
+			return;
+		}
+
+		if (screen[x][y] != prevC) {
+			return;
+		}
+
+		screen[x][y] = newC;
+
+		floodFill(screen, x - 1, y, prevC, newC);
+		floodFill(screen, x + 1, y, prevC, newC);
+		floodFill(screen, x, y + 1, prevC, newC);
+		floodFill(screen, x, y - 1, prevC, newC);
+
+	}
+
+}
